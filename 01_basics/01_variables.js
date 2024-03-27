4673621db933847c41fa4011b09b0e7fc9543f1a
Ravi Kumar Patel <109025108+ravikrpatel01@users.noreply.github.com>
2024-03-27 10:50:01 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 452855
+let accountEmail = "ravi12@google.com"
+var accountPassword = "ravi@12345"
+
+accountCity = "Ramgarh"
+
+// accountId = 45
+
+accountEmail = "vs@shreya.com"
+accountPassword = "shreya@123"
+accountCity = "Ranchi"
+
+let accountState;
+let val = null;
+
+// console.log(accountId);
+// console.log(accountCity);
+
+console.table([accountId, val, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(12 + 25) // we are using nodejs, not browser
+
+console.log(12 + 25)
+
+console.log("Patel");
+
+let name = "ravi"
+let age = 25
+let isLoggedIn = true
+let state = null
+
+/*
+number => 2^53
+bigint
+string = ""
+boolean => true/false
+null => standalone value
+undefined => value not assigned
+symbol => unique in react(e.g)
+*/
+
+/*
+object
+*/
+
+console.log(typeof("patel"));
+console.log(typeof(age));
+console.log(typeof(null));
+console.log(typeof(undefined ));
+
+
+
+// https://www.tc39.es
+// ECMAScript
